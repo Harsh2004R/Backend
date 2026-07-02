c38959761405fea1d8cc794e404af4562a40fa2d
Harsh2004R <harshsharmaktm03@gmail.com>
2026-07-02 12:55:05 +0530

    Configure cloudinary in application

---
+++ utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+// cloudinary configurations 
+cloudinary.config({
+    cloud_name=process.env.CLOUDINARY_CLOUD_NAME,
+    api_key=process.env.CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_SECRET_KEY,
+})
+
+
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null;
+        }
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        console.log(`file successfuly uploaded on cloudinary \n ${response}`)
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // removing file from server 
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
