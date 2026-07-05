1ced4aeebcc1a0df8e8cc9f0e5de33510f0e47a8
Harsh2004R <harshsharmaktm03@gmail.com>
2026-07-05 13:43:24 +0530

    Register Controller final fix for basic signup

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,7 +1,6 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import UserModel from "../model/user.model.js"
-
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 
@@ -14,7 +13,34 @@ const userVerifyController = async (req, res) => {
 
 const userRegisterController = async (req, res) => {
 
-    res.status(200).json({ message: "signup api working..." })
+    const { fullName, email, password } = req.body;
+    try {
+
+        if ([fullName, email, password].some((fields) => fields?.trim() === "" || !fields)) {
+            throw new ApiError(400, "Required fields are missing form request body")
+        }
+        const existedUser = await UserModel.findOne({ $or: [{ fullName }, { email }] });
+        if (existedUser) {
+            throw new ApiError(409, `User Already exist with ${fullName} or ${email}`);
+        }
+
+        const user = new UserModel({
+            fullName,
+            email,
+            password
+        });
+        await user.save();
+        const createdUser = await UserModel.findById(user._id).select("-password -refreshToken")
+        if (!createdUser) {
+            throw new ApiError(400, `User ${fullName} or ${email} not created`);
+        }
+        return res.status(201).json(
+            new ApiResponse(201, "user registered successfully", createdUser)
+        )
+
+    } catch (error) {
+        throw new ApiError(error.statusCode || 500, error.message || `Some error is comming from register user controller`);
+    }
 
 
 }
