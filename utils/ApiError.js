606cd0df72736748311bd07a394ed17bb00b2cf8
Harsh2004R <harshsharmaktm03@gmail.com>
2026-07-05 13:52:39 +0530

    cleanups and error message update

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -1,6 +1,6 @@
 class ApiError extends Error {
     constructor(
-        statusCode = 500, message = "error in server", errors = [],
+        statusCode = 500, message = "Internal Server Error", errors = [],
 
     ) {
         super(message)
@@ -12,11 +12,6 @@ class ApiError extends Error {
 
 
         Error.captureStackTrace(this, this.constructor);
-        // if (stack) {
-        //     this.stack;
-        // } else {
-        //     Error.captureStackTrace(this, this.constructor)
-        // }
     }
 }
 
