bb1ab6740089ed9ded02a6f2219d484451f79230
Harsh2004R <harshsharmaktm03@gmail.com>
2026-07-13 11:07:56 +0530

    Update CORS configuration to specify allowed origins and headers

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(cors(
     {
-        origin: process.env.CORS_ORIGIN || ["http://localhost:8001"],
+        // origin: process.env.CORS_ORIGIN || ["http://localhost:8001"],
+        origin : [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000",
+            "http://localhost:5000",
+            // production url
+        ],
+        allowedHeaders:["Content-Type", "Authorization"],
         credentials: true
     }
 ));
